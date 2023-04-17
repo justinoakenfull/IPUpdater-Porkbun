ddd1a63866de12516d26ce476692c4115a4b850a
Justin Oakenfull <justinoakenfull@gmail.com>
2023-04-18 00:54:34 +1000

    Added app settings check before executing the rest of the program.

--- IPUpdater/Program.cs
+++ IPUpdater/Program.cs
@@ -2,6 +2,40 @@
 using IPUpdater.Services;
 using IPUpdater.Models;
 
+if (ConfigurationManager.AppSettings.Get("apikey") == null ||
+    ConfigurationManager.AppSettings.Get("secretapikey") == null ||
+    ConfigurationManager.AppSettings.Get("DomainToUpdate") == null ||
+    ConfigurationManager.AppSettings.Get("RecordTypeToUpdate") == null ||
+    ConfigurationManager.AppSettings.Get("PorkbunPing") == null ||
+    ConfigurationManager.AppSettings.Get("PorkbunDNSRequest") == null ||
+    ConfigurationManager.AppSettings.Get("PorkbunDNSEdit") == null ||
+    ConfigurationManager.AppSettings.Get("VerboseDebugging") == null ||
+    ConfigurationManager.AppSettings.Get("apikey") == "" ||
+    ConfigurationManager.AppSettings.Get("secretapikey") == "" ||
+    ConfigurationManager.AppSettings.Get("DomainToUpdate") == "" ||
+    ConfigurationManager.AppSettings.Get("RecordTypeToUpdate") == "" ||
+    ConfigurationManager.AppSettings.Get("PorkbunPing") == "" ||
+    ConfigurationManager.AppSettings.Get("PorkbunDNSRequest") == "" ||
+    ConfigurationManager.AppSettings.Get("PorkbunDNSEdit") == "" ||
+    ConfigurationManager.AppSettings.Get("VerboseDebugging") == "")
+    {
+        Console.BackgroundColor = ConsoleColor.Gray;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("========================================================================================================================");
+        Console.WriteLine("|| One or more of the application settings was blank, null or improperly formatted. Please check the App.config file. ||");
+        Console.WriteLine("========================================================================================================================");
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("                                                                                                                        ");
+        Console.WriteLine("                                                                                                                        ");
+        Console.ResetColor();
+        Environment.Exit(2);
+    }
+
 UserSettingsModel userSettings = new UserSettingsModel(ConfigurationManager.AppSettings.Get("apikey"),
                                                        ConfigurationManager.AppSettings.Get("secretapikey"),
                                                        ConfigurationManager.AppSettings.Get("DomainToUpdate"),
